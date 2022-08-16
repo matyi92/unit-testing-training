e8a3dd84622455aa4e4793ead4a9d42988a5129d
mvas <matyas.VAS@frequentis.com>
2022-08-16 16:36:25 +0300

    initial commit

---
+++ unit-testing/src/main/java/calculator/Calculator.java
@@ -0,0 +1,44 @@
+package calculator;
+
+/************************************************************************
+ ** PROJECT:   XVP
+ ** LANGUAGE:  Java, J2SE JDK 1.8
+ **
+ ** COPYRIGHT: FREQUENTIS AG
+ **            Innovationsstrasse 1
+ **            A-1100 VIENNA
+ **            AUSTRIA
+ **            tel +43 1 811 50-0
+ **
+ ** The copyright to the computer program(s) herein
+ ** is the property of Frequentis AG, Austria.
+ ** The program(s) shall not be used and/or copied without
+ ** the written permission of Frequentis AG.
+ **
+ ************************************************************************/
+
+public class Calculator
+{
+   public int sum( final int a, final int b )
+   {
+      return a + b;
+   }
+
+
+   public int minus( final int a, final int b )
+   {
+      return a - b;
+   }
+
+
+   public int divide( final int a, final int b )
+   {
+      return a / b;
+   }
+
+
+   public int multiply( final int a, final int b )
+   {
+      return a * b;
+   }
+}
